97f514198a51481fd0c1f1431ad59ebed450ebdb
Trainings <Trainings@datanova.co.in>
2023-12-12 15:27:18 +0530

    Login Page Created

--- src/Components/DashBoard.js
+++ src/Components/DashBoard.js
@@ -1,8 +1,49 @@
-import React from 'react'
-export default function DashBoard() {
+import React, { useState } from 'react';
+import './LoginPage.css'; // Import your CSS file for styling
+
+const LoginPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Replace this with your actual authentication logic
+    if (username === 'demo' && password === 'password') {
+      alert('Login successful!');
+    } else {
+      alert('Invalid username or password');
+    }
+  };
+
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="login-container">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
     </div>
-  )
-}
+  );
+};
+
+export default LoginPage;
